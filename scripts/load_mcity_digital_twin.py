af55d971a34c22a3c71a777d8d2a8add1fee35e2
Darian Hogue <dhogue@umich.edu>
2024-09-20 14:17:39 -0400

    added load_mcity_digital_twin script

---
+++ scripts/load_mcity_digital_twin.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+
+import glob
+import os
+import sys
+
+try:
+    sys.path.append(
+        glob.glob(
+            "../carla/dist/carla-*%d.%d-%s.egg"
+            % (
+                sys.version_info.major,
+                sys.version_info.minor,
+                "win-amd64" if os.name == "nt" else "linux-x86_64",
+            )
+        )[0]
+    )
+except IndexError:
+    pass
+
+
+import carla
+
+
+client = carla.Client("localhost", 2000)
+client.set_timeout(5.0)
+
+world = client.load_world("McityMap_Main")
+
+print("Welcome To Mcity!")
